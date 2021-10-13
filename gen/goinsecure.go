7facb5c11d04019c59ced1940cba9e419bd9bc81
Eric Daniels <eric@erdaniels.com>
2021-10-13 12:28:12 -0400

    try better goinsecure

--- gen/goinsecure.go
+++ gen/goinsecure.go
@@ -2,6 +2,7 @@ package gen
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/edaniels/golog"
 
@@ -9,9 +10,23 @@ import (
 )
 
 func GoInsecure() {
-	_, vanity, err := govanity.ParseModules()
+	modules, vanity, err := govanity.ParseModules()
 	if err != nil {
 		golog.Global.Fatal(err)
 	}
 	fmt.Printf("%s/*,", vanity)
+	for _, module := range modules {
+		if module.Vanity {
+			continue
+		}
+		resp, err := http.Get(fmt.Sprintf("https://%s", module.RedirectTo))
+		if err != nil {
+			golog.Global.Debugw("error doing HTTP GET", "module", module.RedirectTo, "error", err)
+			continue
+		}
+		if resp.StatusCode == http.StatusOK {
+			continue
+		}
+		fmt.Printf("%s,", module.Name)
+	}
 }
