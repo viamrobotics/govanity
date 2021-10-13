687327c8f3389130dcd8e308ff162fc422ae9abb
Eric Daniels <eric@erdaniels.com>
2021-10-13 16:53:43 -0400

    some fixes

--- gen/hosts.go
+++ gen/hosts.go
@@ -2,6 +2,7 @@ package gen
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/edaniels/golog"
 
@@ -9,7 +10,7 @@ import (
 )
 
 func Hosts() {
-	_, vanity, err := govanity.ParseModules()
+	modules, vanity, err := govanity.ParseModules()
 	if err != nil {
 		golog.Global.Fatal(err)
 	}
@@ -17,4 +18,18 @@ func Hosts() {
 		return
 	}
 	fmt.Printf("127.0.0.1 %s\n", vanity)
+	for _, module := range modules {
+		if module.Vanity {
+			continue
+		}
+		resp, err := http.Get(fmt.Sprintf("https://%s", module.RedirectTo))
+		if err != nil {
+			golog.Global.Debugw("error doing HTTP GET", "module", module.RedirectTo, "error", err)
+			continue
+		}
+		if resp.StatusCode == http.StatusOK {
+			continue
+		}
+		fmt.Printf("%s,", module.Name)
+	}
 }
